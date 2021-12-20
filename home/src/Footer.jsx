b576cc6867b9c564030fa74b189c8a911bd4eb6d
arabambi <arababs2015@gmail.com>
2021-12-20 21:11:10 +0100

    two apps connected

--- home/src/Footer.jsx
+++ home/src/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 
-const Header = () => {
+const Footer = () => {
 
 
           return (
                     <div className='p-5 bg-blue-500 text-white text-3xl font-bold'>
-                              Fidget Spinner World
+                              Only The Best Spinners
                     </div>
           )
 }
 
 
-export default Header
+export default Footer
--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: pdp</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+// import Header from "home/Header";
+const Header = React.lazy(() => import( "home/Header"));
+
+import Footer from "home/Footer";
+
+
+const App = () => {
+	const [showHeader, setShowHeader] = useState(false)
+
+
+	return (
+		<div className="text-3xl mx-auto max-w-6xl">
+			{
+				showHeader &&
+				<Suspense fallback={<div>Loading</div>}>
+					<Header />
+				</Suspense>
+			}
+
+			
+			<button 
+				className="text-3xl p-5"
+				onClick={() => setShowHeader(!showHeader)}
+			>
+				Show The Header
+			</button>
+
+				<div className="my-10">PDP Page Content</div>
+			<Footer />
+		</div>
+	)
+};
+
+
+
 ReactDOM.render(<App />, document.getElementById("app"));
